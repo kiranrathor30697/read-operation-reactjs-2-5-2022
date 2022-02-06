2af3634d3d02a8a234c900ca14e2fc832cc78b3b
“kiranrathor30697” <kiranrathor30697@gmail.com>
2022-02-05 23:05:24 -1100

    read operation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+//import area
+import { Button, Table} from 'react-bootstrap'
 
+//rfc
 function App() {
+//1. state/Hook variable
+
+let getFriend = (e) => {
+  console.log('Click button press');
+
+  //Api Call
+  try {
+      //return po = promise object
+      fetch('http://localhost:1337/api/friends')
+      .then((data)=>{
+        console.log(data)
+      })
+      .then(()=>{
+
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+
+    } catch (error) {
+        console.error(error)
+    }
+      
+
+}
+
+//2.functions
+
+//3.return statements
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center">Read Operations</h1>
+      <Button className="text-center mb-5" onClick={(e)=>{getFriend(e)}} variant="outline-success">Get My Friends</Button>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   );
 }
 
